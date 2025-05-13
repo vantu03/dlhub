480c5bfe567e893102ea6b13e8f9f238eadedb6d
vantu03 <concaoheo203@gmail.com>
2025-05-13 16:36:17 +0700

    ads

--- dltik/urls.py
+++ dltik/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('dlv/', views.dlv, name='dlv'),
     path('gen-token/', views.generate_token_view, name='gen-token'),
     path('proxy-download/', views.proxy_download, name='proxy-download'),
+    path('ads.txt', TemplateView.as_view(template_name="ads.txt", content_type='text/plain'), name="ads_txt"),
 ]
+
