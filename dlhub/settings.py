7f137a574ca4ac7da9003ad3e14c2b0f114dd03d
vantu03 <concaoheo203@gmail.com>
2025-05-16 00:43:54 +0700

    utc hcm

--- dlhub/settings.py
+++ dlhub/settings.py
@@ -87,11 +87,10 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Ho_Chi_Minh'
+USE_TZ = True
 
 USE_I18N = True
-
-USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
