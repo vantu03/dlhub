ecce73d6b6c726daf84ab62c3b256ff9deac6ba4
vantu03 <concaoheo203@gmail.com>
2025-05-22 02:59:17 +0700

    dark

--- dltik/static/js/ckeditor_custom.js
+++ dltik/static/js/ckeditor_custom.js
@@ -1,3 +1,3 @@
-const editorBody = document.querySelector('.cke_source.cke_reset.cke_enable_context_menu.cke_editable.cke_editable_themed.cke_contents_ltr')
+const editorBody = document.querySelector('.cke_source')
 editorBody.setStyle('background-color', '#1e1e2f');
 editorBody.setStyle('color', '#ffffff');
