1fe6a2f084268e459f68137aedb94c2090ed105c
vantu03 <concaoheo203@gmail.com>
2025-05-22 02:57:47 +0700

    tieesp

--- dltik/static/js/ckeditor_custom.js
+++ dltik/static/js/ckeditor_custom.js
@@ -1,10 +1,3 @@
-CKEDITOR.on('instanceReady', function (ev) {
-    console.log("Custom JS loaded for CKEditor:", ev.editor.name);
-
-    // Ví dụ: auto focus
-    ev.editor.focus();
-
-    // Ví dụ: đặt theme nền tối nếu CSS không đủ
-    ev.editor.document.getBody().setStyle('background-color', '#1e1e2f');
-    ev.editor.document.getBody().setStyle('color', '#f0f0f0');
-});
+const editorBody = document.querySelector('.cke_source.cke_reset.cke_enable_context_menu.cke_editable.cke_editable_themed.cke_contents_ltr')
+editorBody.setStyle('background-color', '#1e1e2f');
+editorBody.setStyle('color', '#ffffff');
